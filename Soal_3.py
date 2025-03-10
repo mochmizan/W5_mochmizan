90c11ba366ec6b5e5de2ce019c5e11cc743d170b
Mizan <ghodafail@gmail.com>
2025-03-10 12:52:26 +0700

    Update Soal_3.py

--- Soal_3.py
+++ Soal_3.py
@@ -1,14 +1,18 @@
 #Soal 3
-class persegi:
-  def __init__(self, sisi):
-    self.sisi = int(sisi)
+class Barang:
+  def __init__(self, merek, harga_satuan):
+    self.merek = merek
+    self.harga_satuan = harga_satuan #dalam ribu
 
-  def luas(self):
-    return (self.sisi**2)
+  def __mul__(self, kuantitas):
+    print('Banyaknya penjualan:', kuantitas.qty_jual, 'buah')
+    return self.harga_satuan * kuantitas.qty_jual
 
-  def keliling(self):
-    return (self.sisi*4)
+class Penjualan:
+  def __init__(self, merek, qty_jual):
+    self.merek = merek
+    self.qty_jual = qty_jual
 
-n = int(input("Masukkan sisi Persegi: "))
-print("Luas Persegi:", persegi(n).luas())
-print("Keliling Persegi:", persegi(n).keliling())
+redmi10 = Barang("Redmi10", 2140)
+qty_maret = Penjualan("Redmi10", 20)
+print(f"Total penjualan {redmi10.merek}: {redmi10*qty_maret} ribu")
